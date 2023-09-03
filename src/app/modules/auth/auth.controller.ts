8fda3fc62232100b269fa8fa0ae2856e16e98a40
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-09-03 21:40:07 +0600

    user login updated in authController

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import config from '../../../config';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
+import { ILoginUserResponse } from './auth.interface';
 import { AuthService } from './auth.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -14,6 +16,26 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const userLogin = catchAsync(async (req: Request, res: Response) => {
+  const result = await AuthService.userLogin(req.body);
+
+  const { refreshToken, ...otherData } = result;
+
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+
+  sendResponse<ILoginUserResponse>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User logged in successfully!',
+    data: otherData,
+  });
+});
+
 export const AuthController = {
   insertIntoDB,
+  userLogin,
 };
