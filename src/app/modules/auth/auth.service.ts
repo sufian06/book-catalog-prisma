fd040c6a449df91e730bb517e5d314ef40d5025e
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-09-03 21:27:56 +0600

    user login and token for access added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,8 +2,12 @@
 import { User } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
 import { prisma } from '../../../shared/prisma';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
 
 const insertIntoDB = async (data: User) => {
   const isExist = await prisma.user.findFirst({
@@ -34,6 +38,45 @@ const insertIntoDB = async (data: User) => {
   return withoutPasswoord;
 };
 
+const userLogin = async (data: ILoginUser): Promise<ILoginUserResponse> => {
+  const { email, password } = data;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist');
+  }
+
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Password does not matched');
+  }
+  // access token
+  const accessToken = jwtHelpers.createToken(
+    { email: user.email, role: user.role, id: user.id },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  // refresh token
+  const refreshToken = jwtHelpers.createToken(
+    { email: user.email, role: user.role, id: user.id },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
 export const AuthService = {
   insertIntoDB,
+  userLogin,
 };
