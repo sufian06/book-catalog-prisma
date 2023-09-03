9ac7389ea1eaffb29793f6c7f800c6ecc4543eff
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-09-03 21:00:04 +0600

    find user email and handle if already have email in the database

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,12 +1,37 @@
+/* eslint-disable no-unused-vars */
 import { User } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import { prisma } from '../../../shared/prisma';
 
-const insertIntoDB = async (data: User): Promise<User> => {
-  const result = await prisma.user.create({
-    data,
+const insertIntoDB = async (data: User) => {
+  const isExist = await prisma.user.findFirst({
+    where: {
+      email: data.email,
+    },
   });
 
-  return result;
+  if (isExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'The email is already exist');
+  }
+
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  const result = await prisma.user.create({
+    data: {
+      ...data,
+      password: hashedPassword,
+    },
+  });
+
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Something went wrong!');
+  }
+
+  const { password, ...withoutPasswoord } = result;
+
+  return withoutPasswoord;
 };
 
 export const AuthService = {
