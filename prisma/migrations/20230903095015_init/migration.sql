a05d69c6b31e30b8c50300cec99880b5b6deafe2
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-09-03 18:05:17 +0600

    added all model in the prisma

---
+++ prisma/migrations/20230903095015_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "RoleUser" AS ENUM ('admin', 'customer');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "RoleUser" NOT NULL DEFAULT 'customer',
+    "contactNo" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "profileImg" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
