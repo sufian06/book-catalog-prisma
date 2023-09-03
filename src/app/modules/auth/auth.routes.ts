8cde433adba41c62e3d1ac6e50918bed973dc2d0
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-09-03 21:50:25 +0600

    [DONE] auth service

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -4,5 +4,6 @@ import { AuthController } from './auth.controller';
 const router = express.Router();
 
 router.post('/signup', AuthController.insertIntoDB);
+router.post('/login', AuthController.userLogin);
 
 export const authRoutes = router;
